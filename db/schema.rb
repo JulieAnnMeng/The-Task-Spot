15360769e2feaa701c915a578125d7034de1ced0
Julie Meng <julieannmeng@gmail.com>
2022-03-15 21:20:04 -0400

    removed email columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_06_191659) do
+ActiveRecord::Schema.define(version: 2021_12_29_154404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,8 +42,6 @@ ActiveRecord::Schema.define(version: 2022_01_06_191659) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "email_confirmed", default: false
-    t.string "confirm_token"
   end
 
   add_foreign_key "lists", "users"
